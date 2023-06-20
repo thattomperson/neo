dd5e18e10d5a6a55857e42ecf25ac23a32d6db39
Tom Albrighton <thomas.albrighton@four.io>
2023-06-20 10:01:32 +0930

    Add autocmd to install php extentions

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,15 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local function augroup(name)
+    return vim.api.nvim_create_augroup("four_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = augroup("install_extensions"),
+    callback = function()
+        vim.notify('Installing PHP extensions...', 'Info')
+        io.popen("four-php-dev php:install-extensions")
+        vim.notify('PHP extensions Installed!', 'Info')
+    end,
+})
