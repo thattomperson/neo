d481faed2d14aa1285f07331531ccfaa9a9375de
Tom Albrighton <thomas.albrighton@four.io>
2023-07-14 17:50:42 +0930

    Add indent guides

--- lua/kickstart/plugins/ui/init.lua
+++ lua/kickstart/plugins/ui/init.lua
@@ -298,8 +298,10 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    event = "BufEnter",
     opts = {
       char = '┊',
+      show_current_context = true,
       show_trailing_blankline_indent = false,
     },
   },
