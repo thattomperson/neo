091d77d0d715b5204538ee3ed77595654780ca57
Tom Albrighton <thomas.albrighton@four.io>
2023-07-20 09:20:44 +0930

    Add phpcs.xml

--- lua/kickstart/plugins/editor/lsp.lua
+++ lua/kickstart/plugins/editor/lsp.lua
@@ -1,3 +1,18 @@
+local function is_program_in_path(program_name)
+  local path_separator = vim.fn.has("win32") == 1 and ";" or ":"
+  local path = os.getenv("PATH")
+  local paths = vim.split(path, path_separator)
+
+  for _, p in ipairs(paths) do
+    local program_path = p .. "/" .. program_name
+    if vim.fn.executable(program_path) == 1 then
+      return true
+    end
+  end
+
+  return false
+end
+
 return {
 
   -- NOTE: This is where your plugins related to LSP can be installed.
@@ -18,7 +33,7 @@ return {
       },
     },
     config = function(_, opts)
-      require('mason').setup(opts)
+      require("mason").setup(opts)
 
       vim.api.nvim_create_user_command("MasonInstallAll", function()
         vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
@@ -115,55 +130,55 @@ return {
           vim.api.nvim_create_autocmd("InsertLeave", {
             callback = function()
               if
-                require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-                and not require("luasnip").session.jump_active
-                then
-                  require("luasnip").unlink_current()
-                end
-              end,
-            })
+                  require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                  and not require("luasnip").session.jump_active
+              then
+                require("luasnip").unlink_current()
+              end
+            end,
+          })
+        end,
+      },
+      -- Snippet Engine & its associated nvim-cmp source
+      "saadparwaiz1/cmp_luasnip",
+
+      -- Adds LSP completion capabilities
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    opts = function()
+      local cmp = require("cmp")
+
+      local options = {
+        completion = {
+          completeopt = "menu,menuone",
+        },
+
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
-        -- Snippet Engine & its associated nvim-cmp source
-        "saadparwaiz1/cmp_luasnip",
 
-        -- Adds LSP completion capabilities
-        "hrsh7th/cmp-nvim-lsp",
-      },
-      opts = function()
-        local cmp = require("cmp")
-
-        local options = {
-          completion = {
-            completeopt = "menu,menuone",
-          },
-
-          snippet = {
-            expand = function(args)
-              require("luasnip").lsp_expand(args.body)
-            end,
-          },
-
-          mapping = {
-            ["<C-p>"] = cmp.mapping.select_prev_item(),
-            ["<C-n>"] = cmp.mapping.select_next_item(),
-            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.close(),
-            ["<CR>"] = cmp.mapping.confirm({
-              behavior = cmp.ConfirmBehavior.Insert,
-              select = true,
-            }),
-            ["<Tab>"] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif require("luasnip").expand_or_jumpable() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-              else
-                fallback()
-              end
-            end, {
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif require("luasnip").expand_or_jumpable() then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+            else
+              fallback()
+            end
+          end, {
             "i",
             "s",
           }),
@@ -176,115 +191,121 @@ return {
               fallback()
             end
           end, {
-          "i",
-          "s",
-        }),
-      },
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "nvim_lua" },
-        { name = "path" },
-      },
-    }
-    return options
-  end,
-},
-{
-  "jose-elias-alvarez/null-ls.nvim",
-  event = "VimEnter",
-  config = function(_, opts)
-    local null_ls = require("null-ls")
-
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.diagnostics.cspell,
-        null_ls.builtins.code_actions.cspell,
-        null_ls.builtins.diagnostics.phpcs,
-        null_ls.builtins.formatting.phpcbf,
-        null_ls.builtins.formatting.stylua,
-      },
-    })
-  end,
-},
-{
-  "nvimdev/guard.nvim",
-  enable = false,
-  ft = { "php" },
-  dependencies = {
-    "williamboman/mason.nvim"
+            "i",
+            "s",
+          }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "nvim_lua" },
+          { name = "path" },
+        },
+      }
+      return options
+    end,
   },
-  opts = {
-    fmt_on_save = true
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VimEnter",
+    config = function(_, opts)
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.diagnostics.cspell,
+          null_ls.builtins.code_actions.cspell,
+          null_ls.builtins.diagnostics.phpcs.with({
+            extra_args = { "--standard", vim.fn.expand("~/.config/nvim/data/phpcs.xml") },
+          }),
+          null_ls.builtins.formatting.phpcbf.with({
+            extra_args = { "--standard", vim.fn.expand("~/.config/nvim/data/phpcs.xml") },
+          }),
+          null_ls.builtins.formatting.stylua.with({
+            condition = function()
+              return is_program_in_path("stylua")
+            end,
+          }),
+        },
+      })
+    end,
   },
-  config = function (_, opts)
-    vim.notify("Guard is loading")
+  {
+    "nvimdev/guard.nvim",
+    enable = false,
+    ft = { "php" },
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    opts = {
+      fmt_on_save = true,
+    },
+    config = function(_, opts)
+      vim.notify("Guard is loading")
 
-    local ft = require('guard.filetype')
-    local diag_fmt = require('guard.lint').diag_fmt
+      local ft = require("guard.filetype")
+      local diag_fmt = require("guard.lint").diag_fmt
 
-    ft('php'):fmt('lsp')
-    :lint({
-      cmd = "phpcs",
-      args = { "-", "--report=json" },
-      stdin = true,
-      output_fmt = function (result, buf)
-        vim.notify("PHPCS was RUN")
-        vim.notify(result)
-        local severities = {
-          WARNING = 2,
-          ERROR = 1,
-        }
+      ft("php"):fmt("lsp"):lint({
+        cmd = "phpcs",
+        args = { "-", "--report=json" },
+        stdin = true,
+        output_fmt = function(result, buf)
+          vim.notify("PHPCS was RUN")
+          vim.notify(result)
+          local severities = {
+            WARNING = 2,
+            ERROR = 1,
+          }
 
-        local messages = vim.json.decode(result).files.STDIN.messages
-        local diags = {}
+          local messages = vim.json.decode(result).files.STDIN.messages
+          local diags = {}
 
-        if #messages < 1 then
-          return {}
-        end
+          if #messages < 1 then
+            return {}
+          end
 
-        vim.tbl_map(function(mes)
-          diags[#diags + 1] = diag_fmt(
-          buf,
-          tonumber(mes.line) - 1,
-          tonumber(mes.column) - 1,
-          (mes.fixable and "[x]" or "") .. mes.message,
-          severities[mes.severity] or 2,
-          'phpcs'
-          )
-        end, offenses)
+          vim.tbl_map(function(mes)
+            diags[#diags + 1] = diag_fmt(
+              buf,
+              tonumber(mes.line) - 1,
+              tonumber(mes.column) - 1,
+              (mes.fixable and "[x]" or "") .. mes.message,
+              severities[mes.severity] or 2,
+              "phpcs"
+            )
+          end, offenses)
+        end,
+      })
 
-      end
-    })
-
-    require('guard').setup(opts)
-  end
-},
---{
+      require("guard").setup(opts)
+    end,
+  },
+  --{
   --  url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
   --  ft = { "php" },
   --  dependencies = {
-    --    "williamboman/mason.nvim"
-    --  },
-    --  config = function()
-      --    local sonar_language_server_path = require("mason-registry")
-      --        .get_package("sonarlint-language-server")
-      --        :get_install_path()
-      --    local analyzers_path = sonar_language_server_path .. "/extension/analyzers"
-      --    require("sonarlint").setup({
-        --      server = {
-          --        cmd = {
-            --          sonar_language_server_path .. "/sonarlint-language-server.cmd",
-            --          "-stdio",
-            --          "-analyzers",
-            --          vim.fn.expand(analyzers_path .. "/sonarphp.jar"),
-            --        }
-            --      },
-            --      filetypes = {
-              --        "php",
-              --      }
-              --    })
-              --  end
-              --},
-            }
+  --    "williamboman/mason.nvim"
+  --  },
+  --  config = function()
+  --    local sonar_language_server_path = require("mason-registry")
+  --        .get_package("sonarlint-language-server")
+  --        :get_install_path()
+  --    local analyzers_path = sonar_language_server_path .. "/extension/analyzers"
+  --    require("sonarlint").setup({
+  --      server = {
+  --        cmd = {
+  --          sonar_language_server_path .. "/sonarlint-language-server.cmd",
+  --          "-stdio",
+  --          "-analyzers",
+  --          vim.fn.expand(analyzers_path .. "/sonarphp.jar"),
+  --        }
+  --      },
+  --      filetypes = {
+  --        "php",
+  --      }
+  --    })
+  --  end
+  --},
+}
